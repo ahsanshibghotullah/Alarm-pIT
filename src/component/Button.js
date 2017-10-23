f2018ec48289157dd1ca3efa81729fe2dac399fe
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-23 21:28:27 +0700

    Styling Button

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress, children, styleButton,styleText }) => {
+const Button = ({ onPress, children, styleButton, styleText }) => {
     const { buttonStyle, textStyle } = styles;
     return (
         <TouchableOpacity
