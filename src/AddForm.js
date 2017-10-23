cdd9c0fa88419d878d2e89f08433ddcdc3e44a22
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-23 22:14:36 +0700

    menambah file AddForm.js

---
+++ src/AddForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const AddForm = () => {
+    return (
+        <View>
+            <Text>Add Form</Text>       
+        </View>
+    );
+};
+
+export default AddForm;
