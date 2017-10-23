564f2c82a5b5c6002cbdf908a94226175c152157
Alfiow <alfianan0412@bsi.ac.id>
2017-10-23 21:49:50 +0700

    mengubah App.js dan tambah eslintrc

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -13,7 +7,7 @@ import {
 } from 'react-native';
 
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
