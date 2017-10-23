2dd58a43cc5f9b2adc58b93c0fb4ce501e0848ed
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-23 21:35:55 +0700

    Menambahkan file render List yaitu List.js

---
+++ src/List.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const List = () => {
+    const { story, storyTextt } = styles;
+    return (
+        <View style={story}>
+            <Text style={storyText}>
+                {mystory}
+            </Text>
+        </View>
+    );
+}
+
+const styles = {
+    story: {
+        position: 'relative',
+        padding: 20,
+        paddingRight: 100,
+        borderBottomWidth: 2,
+        borderBottomColor: 'black',
+        flexDirection: 'column'
+    },
+    storyText: {
+        paddingLeft: 20,
+        borderLeftWidth: 10,
+        borderLeftColor: 'black',
+        fontSize: 18
+    },
+};
+
+export default List;
