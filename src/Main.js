b028fe6091f9a5ad1c2ccf6231a0c99178668eb2
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-23 21:11:43 +0700

    Membuat index untuk reusable Component

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Button from './component/Button';
+import { Button } from './component';
 
 class Main extends Component {
     render() {
