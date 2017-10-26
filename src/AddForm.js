766402a35007ec159233b19b4fdabadb5f0953b2
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-26 08:50:36 +0700

    Membuat prop AddForm.js

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,58 +1,54 @@
 import React from 'react';
-import { View, Picker, Modal } from 'react-native';
+import { View, Picker } from 'react-native';
 import { Field, Button } from './component';
 
-const AddForm = ({ visible, value, onChangeText, onPress }) => {
+const AddForm = ({ 
+    value, onChangeText, onPress, 
+    selectedValueHour, onValueChangeHour, selectedValueMinute, onValueChangeMinute
+                }) => {
     const { container, styleButton, pickerStyle, wrapPicker } = styles;
     return (
-        <Modal
-        visible={visible}
-        transparent
-        animationType="slide"
-        onRequestClose={() => {}}
-        >
-            <View style={container}>
-                <View>
-                    <Field 
-                    label="Label"
-                    placeholder="..."
-                    autoCorrect={true}
-                    onChangeText={onChangeText}
-                    value={value}
-                    />
-                </View>
-
-                <View style={wrapPicker}>
-                    <Picker
-                    selectedValue={this.state.date}
-                    onValueChange={date => this.setState({ date })}
-                    style={pickerStyle}
-                    >
-                        <Picker.Item label="1" value="1" />
-                        <Picker.Item label="2" value="2" />
-                        <Picker.Item label="3" value="3" />
-                        <Picker.Item label="4" value="4" />
-                    </Picker>
-                    <Picker
-                    selectedValue={this.state.date2}
-                    onValueChange={date2 => this.setState({ date2 })}
-                    style={pickerStyle}
-                    >
-                        <Picker.Item label="1" value="1" />
-                        <Picker.Item label="2" value="2" />
-                        <Picker.Item label="3" value="3" />
-                        <Picker.Item label="4" value="4" />
-                    </Picker>
-                </View>
-                {/* tombol buat nambahin list */}
-                <Button
-                onPress={this.onButtonPress.bind(this)}
-                styleButton={styleButton}
-                >
-                    Add
-                </Button>
+        <View style={container}>
+            <View>
+                <Field 
+                label="Label"
+                placeholder="..."
+                autoCorrect={true}
+                onChangeText={onChangeText}
+                value={value}
+                />
             </View>
-        </Modal>
+
+            <View style={wrapPicker}>
+                <Picker
+                selectedValue={selectedValueHour}
+                onValueChange={onValueChangeHour}
+                style={pickerStyle}
+                >
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                </Picker>
+                <Picker
+                selectedValue={selectedValueMinute}
+                onValueChange={onValueChangeMinute}
+                style={pickerStyle}
+                >
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                </Picker>
+            </View>
+            {/* tombol buat nambahin list */}
+            <Button
+            onPress={onPress}
+            styleButton={styleButton}
+            >
+                Add
+            </Button>
+        </View>
     );
 };
 
