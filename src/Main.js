df62ba1d91a3c03ac29383584859e90e187bb417
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-26 09:54:51 +0700

    menambahkan asynstorage

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,79 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, AsyncStorage, Text, ListView } from 'react-native';
 import { Button } from './component';
 import AddForm from './AddForm';
 
 class Main extends Component {
-    state ={
-        showModal: false
+    constructor(props) {
+        super(props);
+    
+        const ds = new ListView.DataSource({
+          rowHasChanged: (r1, r2) => r1 !== r2
+        });
+    
+        this.state = {
+          ds: new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+          }),
+          listOfTasks: [],
+          text: '',
+          showModal: false
+        };
+      }
+
+    componentDidMount() {
+        this.onUpdateList();
     }
 
     onAddPress() {
-        this.setState({ showModal: false })
+        this.setState({ showModal: false });
+        this.onAddTask();
+    }
+
+    async onAddTask() {
+        const listOfTasks = [...this.state.listOfTasks, this.state.text];
+    
+        await AsyncStorage.setItem('listOfTasks', JSON.stringify(listOfTasks));
+    
+        this.onUpdateList();
+    }
+
+    async onUpdateList() {
+        const response = await AsyncStorage.getItem('listOfTasks');
+        const listOfTasks = await JSON.parse(response) || [];
+    
+        this.setState({
+          listOfTasks
+        });
+    
+        this.onChangeTextInputValue('');
+      }
+
+    onChangeTextInputValue(text) {
+        this.setState({
+          text
+        });
+    }
+
+    renderRowData(rowData) {
+        return (
+          <Text>{ rowData }</Text>
+        );
     }
 
     render() {
+        const dataSource = this.state.ds.cloneWithRows(this.state.listOfTasks);
        const { containerStyle, styleWrapButton, styleButton, styleText } = styles;
+
         return (
             <View style={containerStyle}>
                 <View style={styleWrapButton}>
                     {/* tombol buat pindah ke Add Form */}
+                    <ListView
+                    dataSource={dataSource}
+                    enableEmptySections
+                    renderRow={rowData => this.renderRowData(rowData)}
+                    />
                     <Button
                     styleButton={styleButton}
                     styleText={styleText}
@@ -28,6 +84,8 @@ class Main extends Component {
                     <AddForm 
                     visible={this.state.showModal}
                     onPress={this.onAddPress.bind(this)}
+                    onChangeText={text => this.onChangeTextInputValue(text)}
+                    value={this.state.text}
                     />
                 </View>
             </View>
