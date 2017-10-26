8f5816e0a708e0ac6152e1b647e4f2c7cb53f628
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-10-26 09:08:00 +0700

    Menambah view background

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,54 +1,63 @@
 import React from 'react';
-import { View, Picker } from 'react-native';
+import { View, Picker, Modal } from 'react-native';
 import { Field, Button } from './component';
 
 const AddForm = ({ 
-    value, onChangeText, onPress, 
+    visible, value, onChangeText, onPress, 
     selectedValueHour, onValueChangeHour, selectedValueMinute, onValueChangeMinute
                 }) => {
-    const { container, styleButton, pickerStyle, wrapPicker } = styles;
+    const { container, background, styleButton, pickerStyle, wrapPicker } = styles;
     return (
-        <View style={container}>
-            <View>
-                <Field 
-                label="Label"
-                placeholder="..."
-                autoCorrect={true}
-                onChangeText={onChangeText}
-                value={value}
-                />
-            </View>
+        <Modal
+        visible={visible}
+        transparent
+        animationType="slide"
+        onRequestClose={() => {}}
+        >
+            <View style={container}>
+                <View style={background}>
+                    <View>
+                        <Field 
+                        label="Label"
+                        placeholder="..."
+                        autoCorrect={true}
+                        onChangeText={onChangeText}
+                        value={value}
+                        />
+                    </View>
 
-            <View style={wrapPicker}>
-                <Picker
-                selectedValue={selectedValueHour}
-                onValueChange={onValueChangeHour}
-                style={pickerStyle}
-                >
-                    <Picker.Item label="1" value="1" />
-                    <Picker.Item label="2" value="2" />
-                    <Picker.Item label="3" value="3" />
-                    <Picker.Item label="4" value="4" />
-                </Picker>
-                <Picker
-                selectedValue={selectedValueMinute}
-                onValueChange={onValueChangeMinute}
-                style={pickerStyle}
-                >
-                    <Picker.Item label="1" value="1" />
-                    <Picker.Item label="2" value="2" />
-                    <Picker.Item label="3" value="3" />
-                    <Picker.Item label="4" value="4" />
-                </Picker>
+                    <View style={wrapPicker}>
+                        <Picker
+                        selectedValue={selectedValueHour}
+                        onValueChange={onValueChangeHour}
+                        style={pickerStyle}
+                        >
+                            <Picker.Item label="1" value="1" />
+                            <Picker.Item label="2" value="2" />
+                            <Picker.Item label="3" value="3" />
+                            <Picker.Item label="4" value="4" />
+                        </Picker>
+                        <Picker
+                        selectedValue={selectedValueMinute}
+                        onValueChange={onValueChangeMinute}
+                        style={pickerStyle}
+                        >
+                            <Picker.Item label="1" value="1" />
+                            <Picker.Item label="2" value="2" />
+                            <Picker.Item label="3" value="3" />
+                            <Picker.Item label="4" value="4" />
+                        </Picker>
+                    </View>
+                    {/* tombol buat nambahin list */}
+                    <Button
+                    onPress={onPress}
+                    styleButton={styleButton}
+                    >
+                        Add
+                    </Button>
+                </View>
             </View>
-            {/* tombol buat nambahin list */}
-            <Button
-            onPress={onPress}
-            styleButton={styleButton}
-            >
-                Add
-            </Button>
-        </View>
+        </Modal>
     );
 };
 
@@ -60,6 +69,9 @@ const styles = {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    background: {
+        backgroundColor: 'white'
     },
     wrapTextInput: {
        borderWidth: 1,
